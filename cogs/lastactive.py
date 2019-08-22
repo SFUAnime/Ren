a4785abf336756c7d56c8fec2fc993572c645e36
Jan <42362229+jangarong@users.noreply.github.com>
2019-08-22 10:46:46 -0400

    added docstring and camelcase

--- cogs/lastactive.py
+++ cogs/lastactive.py
@@ -2,8 +2,8 @@
 lastactive (Ren v2) - Tracks when a user was last active.
 
 How to use:
-    bot.last_active[server_id][channel_id][author_id] -> Retrieves datetime (UTC) of last message sent by that author in
-    that channel in that server.
+    bot.last_active[server_id][channel_id][author_id] -> Retrieves datetime (UTC) of last message
+    sent by that author in that channel in that server.
 
 Last updated by jangarong on August 22nd, 2019.
 """
@@ -15,82 +15,91 @@ from datetime import datetime, timedelta
 
 class LastActive:
 
-    """
-    from_json : (Boolean) If true, it will retrieve last active data from json based on path. If false, it will
-        retrieve last active data via logs from channels.
-    to_json : (Boolean) If true, it will save the json file in the desired path periodically.
-    limit : (Integer) Number of messages read in the chat history of each channel. Keep in mind that the larger the
-        number, the longer it will take for the bot to set up.
+    def __init__(self, bot, fromJson=False, toJson=True, limit=500):
+        """
+        fromJson : (Boolean) If true, it will retrieve last active data from json based on path. If
+            false, it will retrieve last active data via logs from channels.
+        toJson : (Boolean) If true, it will save the json file in the desired path periodically.
+        limit : (Integer) Number of messages read in the chat history of each channel. Keep in mind
+            that the larger the number, the longer it will take for the bot to set up.
 
-    To change these values, see the setup function down below.
-    """
-    def __init__(self, bot, from_json=False, to_json=True, limit=500):
+        To change these values, see the setup function down below.
+        """
         self.bot = bot
-        self.bot.last_active = {}
-        self.from_json = from_json
-        self.to_json = to_json
+        self.bot.lastActive = {}
+        self.fromJson = fromJson
+        self.toJson = toJson
         self.limit = limit
 
         # working directory = cogs
-        self.json_path = os.path.abspath(os.path.dirname(__file__))[:-len('/cogs')] + '/data/lastactive/' \
-                                                                                      'last_active.json'
+        self.jsonPath = os.path.abspath(os.path.dirname(__file__))[:-len('/cogs')] + \
+                        '/data/lastactive/last_active.json'
 
         # create loop that goes every 15 minutes
         self.lastChecked = datetime.now() - timedelta(minutes=15)
-        self.bgTask = self.bot.loop.create_task(self.json_loop())
+        self.bgTask = self.bot.loop.create_task(self.jsonLoop())
 
-    def create_folder(self):
-        folder_name = self.json_path[:-len('last_active.json')]
+    def createFolder(self):
+        """Creates a folder in case if one did not exist already."""
+        folder_name = self.jsonPath[:-len('last_active.json')]
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
 
-    def _to_json(self):
-        self.create_folder()
-        with open(self.json_path, 'w') as f:
-            json.dump(self.bot.last_active, f)
+    def dumpJson(self):
+        """Saves dictionary into json."""
+        self.createFolder()
+        with open(self.jsonPath, 'w') as f:
+            json.dump(self.bot.lastActive, f)
 
-    def _load_json(self):
-        with open(self.json_path, 'r') as f:
-            self.bot.last_active = json.load(f)
+    def loadJson(self):
+        """Loads dictionary into json."""
+        with open(self.jsonPath, 'r') as f:
+            self.bot.lastActive = json.load(f)
 
     # iterate per day to save last active data
-    async def json_loop(self):
+    async def jsonLoop(self):
+        """Saves dictionary into json file every 15 minutes."""
         while self == self.bot.get_cog("LastActive"):
             if (self.lastChecked + timedelta(minutes=15)).minute == datetime.now().minute:
                 self.lastChecked = datetime.now()
-                self._load_json()
+                self.dumpJson()
             await asyncio.sleep(60)
 
-    def add_to_dict(self, message):
-
+    def addToDict(self, message):
+        """Retrieves metadata from the message and places it accordingly in the dictionary."""
         if message.author.id != self.bot.user.id:
 
             # if the server exists
-            if message.server.id in self.bot.last_active:
+            if message.server.id in self.bot.lastActive:
 
                 # update timestamp if channel exists
-                if message.channel.id in self.bot.last_active[message.server.id]:
-                    self.bot.last_active[message.server.id][message.channel.id][message.author.id] = message.timestamp
+                if message.channel.id in self.bot.lastActive[message.server.id]:
+                    self.bot.lastActive[message.server.id][message.channel.id][message.author.id] \
+                        = message.timestamp
 
                 # if the channel did not exist before
                 else:
-                    self.bot.last_active[message.server.id][message.channel.id] = {}
-                    self.bot.last_active[message.server.id][message.channel.id][message.author.id] = message.timestamp
+                    self.bot.lastActive[message.server.id][message.channel.id] = {}
+                    self.bot.lastActive[message.server.id][message.channel.id][message.author.id] \
+                        = message.timestamp
 
             # if the server did not exist before
             else:
-                self.bot.last_active[message.server.id] = {}
-                self.bot.last_active[message.server.id][message.channel.id] = {}
-                self.bot.last_active[message.server.id][message.channel.id][message.author.id] = message.timestamp
+                self.bot.lastActive[message.server.id] = {}
+                self.bot.lastActive[message.server.id][message.channel.id] = {}
+                self.bot.lastActive[message.server.id][message.channel.id][message.author.id] = \
+                    message.timestamp
 
     async def on_ready(self):
-
+        """Depending on the parameters given in __init__, it will either try
+        to read from an existing json file, and use that as a dictionary, or read
+        n messages from every text channel."""
         # load json
-        if self.from_json:
+        if self.fromJson:
             try:
-                self._load_json()
+                self.loadJson()
             except FileNotFoundError:
-                self.create_folder()
+                self.createFolder()
 
         else:
 
@@ -103,14 +112,15 @@ class LastActive:
 
                         # go through each message and add them to dictionary
                         async for message in self.bot.logs_from(channel, limit=self.limit):
-                            self.add_to_dict(message)
+                            self.addToDict(message)
 
     # update dictionary with latest post
     async def listener(self, message):
-        self.add_to_dict(message)
+        """For every message, add to dictionary."""
+        self.addToDict(message)
 
 
 def setup(bot):
-    n = LastActive(bot, to_json=True)
+    n = LastActive(bot, toJson=True)
     bot.add_listener(n.listener, "on_message")
     bot.add_cog(n)
