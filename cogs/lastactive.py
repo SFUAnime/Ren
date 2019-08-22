9abdc53c11511bba37f8aea185386ea48a028b62
Jan <42362229+jangarong@users.noreply.github.com>
2019-08-22 09:10:52 -0400

    uses json instead of pickle

--- cogs/lastactive.py
+++ cogs/lastactive.py
@@ -8,44 +8,44 @@ How to use:
 Last updated by jangarong on August 22nd, 2019.
 """
 import asyncio
-import pickle
+import json
 from datetime import datetime, timedelta
 
 
 class LastActive:
 
     """
-    pickle_path : (String) Where the pickled file is saved/loaded.
-    from_pickle : (Boolean) If true, it will retrieve last active data from pickle based on path. If false, it will
+    json_path : (String) Where the json file is saved/loaded.
+    from_json : (Boolean) If true, it will retrieve last active data from json based on path. If false, it will
         retrieve last active data via logs from channels.
-    to_pickle : (Boolean) If true, it will pickle the last active data and save it in the desired path periodically.
+    to_json : (Boolean) If true, it will save the json file in the desired path periodically.
     limit : (Integer) Number of messages read in the chat history of each channel. Keep in mind that the larger the
         number, the longer it will take for the bot to set up.
 
     To change these values, see the setup function down below.
     """
-    def __init__(self, bot, pickle_path='last_active.pkl', from_pickle=False, to_pickle=True, limit=500):
+    def __init__(self, bot, json_path='/cogs/lastactive/last_active.json', from_json=False, to_json=True, limit=500):
         self.bot = bot
         self.bot.last_active = {}
-        self.pickle_path = pickle_path
-        self.from_pickle = from_pickle
-        self.to_pickle = to_pickle
+        self.json_path = json_path
+        self.from_json = from_json
+        self.to_json = to_json
         self.limit = limit
 
         # create loop that goes per day
         self.lastChecked = datetime.now() - timedelta(days=1)
-        self.bgTask = self.bot.loop.create_task(self.pickle_loop())
+        self.bgTask = self.bot.loop.create_task(self.json_loop())
 
-    def _to_pickle(self):
-        with open(self.pickle_path, 'wb') as f:
-            pickle.dump(self.bot.last_active, f, pickle.HIGHEST_PROTOCOL)
+    def _to_json(self):
+        with open(self.json_path, 'w') as f:
+            json.dump(self.bot.last_active, f)
 
-    def _load_pickle(self):
-        with open(self.pickle_path, 'rb') as f:
-            self.bot.last_active = pickle.load(f)
+    def _load_json(self):
+        with open(self.json_path, 'r') as f:
+            self.bot.last_active = json.load(f)
 
     # iterate per day to save last active data
-    async def pickle_loop(self):
+    async def json_loop(self):
         while self == self.bot.get_cog("LastActive"):
             if self.lastChecked.day != datetime.now().day:
                 self.lastChecked = datetime.now()
@@ -53,7 +53,7 @@ class LastActive:
             await asyncio.sleep(60)
 
     async def save_data(self):
-        await self._to_pickle()
+        await self._to_json()
 
     def add_to_db(self, message):
 
@@ -77,13 +77,12 @@ class LastActive:
                 self.bot.last_active[message.server.id][message.channel.id] = {}
                 self.bot.last_active[message.server.id][message.channel.id][message.author.id] = message.timestamp
 
-    # limit = number of messages to read in each channel (the larger the number, the longer it'll take to load)
-    async def on_ready(self, limit=9999):
+    async def on_ready(self):
 
-        # load pickle
-        if self.from_pickle:
+        # load json
+        if self.from_json:
             try:
-                self._load_pickle()
+                self._load_json()
             except FileNotFoundError:
                 pass
 
@@ -97,7 +96,7 @@ class LastActive:
                     if str(channel.type) == 'text':
 
                         # go through each message and add them to dictionary
-                        async for message in self.bot.logs_from(channel, limit=limit):
+                        async for message in self.bot.logs_from(channel, limit=self.limit):
                             self.add_to_db(message)
 
     # update dictionary with latest post
