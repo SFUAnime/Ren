f2848680852c825f005c26177383664895aaa358
jacob <jacoblacsamana0@gmail.com>
2021-08-26 19:19:51 -0700

    rework _filterword() to do nothing when no filters yet

--- cogs/wordfilter/wordfilter.py
+++ cogs/wordfilter/wordfilter.py
@@ -559,13 +559,18 @@ def _censorMatch(matchobj):
 
 
 def _filterWord(words, string):
-    # Combine all filter words into one regex
-    numFilters = len(words) - 1
-    reFormat = r"\b(?:" + (r"{}|") * numFilters + r"{})\b"
-    regex = reFormat.format(*words)
+    numWords = len(words)
+    if numWords < 1:
+        # if no filters added yet, do nothing
+        pass
+    else:
+        # Combine all filter words into one regex
+        numFilters = numWords - 1
+        reFormat = r"\b(?:" + (r"{}|") * numFilters + r"{})\b"
+        regex = reFormat.format(*words)
 
-    # Replace the offending string with the correct number of stars.
-    return re.sub(regex, _censorMatch, string, flags=re.IGNORECASE)
+        # Replace the offending string with the correct number of stars.
+        return re.sub(regex, _censorMatch, string, flags=re.IGNORECASE)
 
 
 def _isOneWord(string):
