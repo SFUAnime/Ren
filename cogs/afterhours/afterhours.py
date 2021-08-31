24fd59121597de7998ae817f5077c0d53f892689
quachtridat <quachtridat@yahoo.com.vn>
2021-08-30 19:18:16 -0700

    [AfterHours] Reformat with `black`

--- cogs/afterhours/afterhours.py
+++ cogs/afterhours/afterhours.py
@@ -232,7 +232,9 @@ class AfterHours(commands.Cog):
         try:
             starboard = sbCog.starboards[ctx.guild.id]["highlights"]
             if remove:
-                await ctx.invoke(sbCog.blacklist_remove, starboard=starboard, channel_or_role=channel)
+                await ctx.invoke(
+                    sbCog.blacklist_remove, starboard=starboard, channel_or_role=channel
+                )
             else:
                 await ctx.invoke(sbCog.blacklist_add, starboard=starboard, channel_or_role=channel)
         except KeyError:
