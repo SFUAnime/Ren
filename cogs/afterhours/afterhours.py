ba36f3ad2fd023d23d16f5ac213c68d8761be3a8
quachtridat <quachtridat@yahoo.com.vn>
2021-08-30 20:30:37 -0700

    [AfterHours] Move `ctx.invoke`s into `else` block
    Move `ctx.invoke` calls into `try/catch/else`'s `else` block.
    This leaves room for more exception handling to be coded if any,
    and leaves `try` block specific for obtaining `starboard`.

--- cogs/afterhours/afterhours.py
+++ cogs/afterhours/afterhours.py
@@ -231,14 +231,13 @@ class AfterHours(commands.Cog):
 
         try:
             starboard = sbCog.starboards[ctx.guild.id]["highlights"]
-            if remove:
-                await ctx.invoke(
-                    sbCog.blacklist_remove, starboard=starboard, channel_or_role=channel
-                )
-            else:
-                await ctx.invoke(sbCog.blacklist_add, starboard=starboard, channel_or_role=channel)
         except KeyError:
             self.logger.error("Cannot get the starboard!")
+        else:
+            if remove:
+                await ctx.invoke(sbCog.blacklist_remove, starboard=starboard, channel_or_role=channel)
+            else:
+                await ctx.invoke(sbCog.blacklist_add, starboard=starboard, channel_or_role=channel)
 
     async def notifyChannel(self, ctx, remove=False):
         if remove:
