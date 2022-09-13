e18247befde49c47668169eaff3cd4f158aec4ae
Jacob <jacoblacsamana0@gmail.com>
2022-09-13 00:06:11 -0700

    Revert "black reformat"
    This reverts commit 6b7d0847e35bb5f1641baab9f057df102532bb4f.

--- cogs/welcome/welcome.py
+++ cogs/welcome/welcome.py
@@ -177,6 +177,7 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
 
         message = rawMessage.replace("{USER}", newUser.mention)
 
+
         try:
             img = await self.generateRandWelcomeImg(newUser)
             if await self.config.guild(guild).get_attr("toggle_img")():
@@ -530,12 +531,11 @@ class Welcome(commands.Cog):  # pylint: disable=too-many-instance-attributes
             if guildData["toggle_img"]:
                 guildData["toggle_img"] = False
             elif guildData["toggle_img"] == False:
-                # check if there is at least one image in the pool at least, otherwise tell user to add one before enabling
+                #check if there is at least one image in the pool at least, otherwise tell user to add one before enabling 
                 if len(os.listdir(self.img_dir)) < 1:
-                    await ctx.send(
-                        "There are currently no images in the image_base folder. Add at least one before turning the randomiser on"
-                    )
-                    return
+                    await ctx.send("There are currently no images in the image_base folder. Add at least one before turning the randomiser on")
+                    return                
+
 
                 guildData["toggle_img"] = True
 
