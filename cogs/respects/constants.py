0873ac30108c490333b74872155cc9a86524bb8a
octtariv <128827755+octtariv@users.noreply.github.com>
2023-03-31 18:25:29 -0700

    [Respects] Reorganize constants

--- cogs/respects/constants.py
+++ cogs/respects/constants.py
@@ -1,11 +1,6 @@
 from typing import Final, List, Optional, Tuple, TypedDict
 
 
-KEY_MSG: Final[str] = "msg"
-KEY_TIME: Final[str] = "time"
-KEY_USERS: Final[str] = "users"
-KEY_TIME_BETWEEN: Final[str] = "timeSinceLastRespect"
-KEY_MSGS_BETWEEN: Final[str] = "msgsSinceLastRespect"
 HEARTS: Final[Tuple[str, ...]] = (
     ":green_heart:",
     ":heart:",
@@ -14,6 +9,11 @@ HEARTS: Final[Tuple[str, ...]] = (
     ":purple_heart:",
     ":blue_heart:",
 )
+
+
+# Guild-related
+KEY_TIME_BETWEEN: Final[str] = "timeSinceLastRespect"
+KEY_MSGS_BETWEEN: Final[str] = "msgsSinceLastRespect"
 DEFAULT_TIME_BETWEEN: Final[float] = 30.0  # Time between paid respects in seconds
 DEFAULT_MSGS_BETWEEN: Final[int] = 20  # The number of messages in between
 
@@ -27,6 +27,12 @@ BASE_GUILD: Final[BaseGuild] = {
     KEY_TIME_BETWEEN: DEFAULT_TIME_BETWEEN,
     KEY_MSGS_BETWEEN: DEFAULT_MSGS_BETWEEN,
 }
+
+
+# Channel-related
+KEY_MSG: Final[str] = "msg"
+KEY_TIME: Final[str] = "time"
+KEY_USERS: Final[str] = "users"
 
 
 class BaseChannel(TypedDict):
