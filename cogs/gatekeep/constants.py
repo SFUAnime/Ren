fe692380df62e47237ed23e879f60f780b44c4fd
Tsunderarislime <terrarislime@gmail.com>
2025-02-04 00:05:24 -0800

    [Gatekeep] Initial commit of cog, should be ready to deploy

---
+++ cogs/gatekeep/constants.py
@@ -0,0 +1,20 @@
+KEY_MSG_BEFORE = "addedBefore"
+KEY_LOG_CHANNEL = "logChannel"
+KEY_JOIN_TIME = "joinTime"
+KEY_THRESHOLD = "threshold"
+KEY_ACTIVE = "active"
+KEY_WORD_DICT = "wordDict"
+KEY_WATCH_LIST = "watchList"
+
+BASE_GUILD_MEMBER = {
+    KEY_MSG_BEFORE: False,
+    KEY_JOIN_TIME: None
+}
+
+BASE_GUILD = {
+    KEY_LOG_CHANNEL: None,
+    KEY_THRESHOLD: 1000000,
+    KEY_ACTIVE: False,
+    KEY_WORD_DICT: {},
+    KEY_WATCH_LIST: []
+}
